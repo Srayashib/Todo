6fb1b3abe68ec6aa5908f6c4281902bbd4601532
Srayashib <srayashibasu@gmail.com>
2023-12-15 19:29:32 +0530

    Todo

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  /*let footerstyle={
+    position:"relative",
+    top:"70vh",
+    width:"100%",
+    border:"2px solid green"
+  }*/
+  return (
+    <footer className="bg-dark text-light py-3" >
+      <p className="text-center">
+        Copyright &copy; MyTodoList.com
+      </p>
+    </footer>
+  )
+}
